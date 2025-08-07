dcf90629f9aa4e91b9429c3b7250c04ae591d953
Ogunderu Noah <ifeoluwadayonoah.com>
2025-08-07 15:01:43 +0100

    SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { MainAppLayout } from "@/components/layout/mainapp";
 import { Public_Sans } from 'next/font/google';
@@ -9,6 +8,14 @@ const publicSans = Public_Sans({
   weight: ['300', '400', '500', '600', '700'],
 })
 
+export const metadata: Metadata = {
+  title: "Fintrack",
+  description: "Track your finances smartly",
+  icons: {
+    icon: "/favicon.png",
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -17,6 +24,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.png" />
+        <title>Fintrack</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
